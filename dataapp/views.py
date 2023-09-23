413adf2f7e4604b5b7799c8238762f032b195533
yewon <ye7648@gmail.com>
2023-09-23 17:09:09 +0900

    accuracy

--- dataapp/views.py
+++ dataapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import View, TemplateView
+from introapp.models import Complete
 
 
 class dataTemplateView(TemplateView):
@@ -10,3 +11,12 @@ class dataTemplateView(TemplateView):
 class megacoffeeView(TemplateView):
     template_name = 'dataapp/megacoffee.html'
 
+    def get(self, request):
+        completes = Complete.objects.all()
+        accuracy_list = []
+        totalTime_list = []
+        for complete in completes:
+            accuracy_list.append(int(complete.accuracy))
+            totalTime_list.append(int(complete.totalTime))
+        context = {'accuracy_list': accuracy_list, 'totalTime_list':totalTime_list}
+        return render(request, 'dataapp/megacoffee.html', context)
